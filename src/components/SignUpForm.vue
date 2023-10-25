598be154144d3287bf9509ac444fff4ba7859b7b
hugoresende <hugo.resende@iservices.pt>
2023-10-25 20:01:04 +0100

    form submit

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,9 +1,13 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
+
     <label>Email</label>
     <input type="email" required v-model="email">
+
     <label>Password</label>
     <input type="password" required v-model="pass">
+    <div v-if="passwordError" class="error"> {{ passwordError }} </div>
+
     <label>Role:</label>
     <select v-model="role">
       <option value="dev">Web Developer</option>
@@ -13,8 +17,11 @@
     <br>
     <label>Skills</label>
     <input type="text" v-model="tempSkill" @keyup="addSkill">
+    
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{ skill }}
+      <span @click="deleteSkill(skill)">
+        {{ skill }}
+      </span>
     </div>
 
 
@@ -23,6 +30,10 @@
         <input type="checkbox" required v-model="terms">
         Accept Terms and conditions
       </label>
+    </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
     </div>
 
     <div class="">
@@ -59,18 +70,19 @@
 export default {
   data() {
     return {
-      email: 'Mario',
+      email: 'Mario@com',
       pass: '',
       role: 'designer',
       terms: false,
       names : [],
       tempSkill: '',
-      skills: []
+      skills: [],
+      passwordError : ''
     }
   },
   methods: {
     addSkill(e) {
-      console.log(e)
+      // console.log(e)
       if( e.key === ',' && this.tempSkill) {
         if (!this.skills.includes(this.tempSkill)){
           this.tempSkill = this.tempSkill.slice(0, -1);
@@ -79,6 +91,25 @@ export default {
         this.tempSkill= ''
       }
     },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        console.log(skill)
+        return skill !== item
+      })
+    },
+    handleSubmit() {
+      console.log('form submited')
+      //validate password
+      this.passwordError = this.pass.length >= 5 ? '' : 'Password minimum 5 chars'
+      if (!this.passwordError) {
+        console.log(this.email)
+        console.log(this.pass)
+        console.log(this.role)
+        console.log(this.skills)
+        console.log(this.terms)
+      }
+
+    }
   },
 }
 </script>
@@ -117,5 +148,41 @@ export default {
       margin: 0 10px 0 0;
       position: relative;
       top: 2px;
+    }
+    .pill {
+      margin: 20px 10px 0 0;
+      display: inline-block;
+      padding: 6px 12px;
+      background: #eee;
+      border-radius: 20px;
+      font-size: 12px;
+      letter-spacing: 1px;
+      font-weight: bold;
+      color: #777;
+      cursor: pointer;
+    }
+
+    button {
+      background: #0b6dff;
+      border: 0;
+      padding: 10px 20px;
+      margin-top: 20px;
+      color: white;
+      border-radius: 20px;
+    }
+    .submit {
+      text-align: center;
+    }
+    .error {
+      margin: 20px 10px 0 0;
+      display: inline-block;
+      padding: 6px 12px;
+      background: red;
+      border-radius: 20px;
+      font-size: 12px;
+      letter-spacing: 1px;
+      font-weight: bold;
+      color: white;
+      cursor: pointer;
     }
 </style>
